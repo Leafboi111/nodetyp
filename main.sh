d2e74a58f901acd4bb4c61004c19510bbc1538fc
Leafboi <110484176+Leafboi111@users.noreply.github.com>
2022-11-24 11:02:32 -0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -6,9 +6,7 @@ typearr=("We're careful about orange ping pong balls because people might think 
 " "He used to get confused between soldiers and shoulders, but as a military man, he now soldiers responsibility.
 " "The sky is clear; the stars are twinkling." "25 years later, she still regretted that specific moment." "I cheated while playing the darts tournament by using a longbow.")
 
-type=$(printf "%s\n" "${typearr[@]}" | shuf -n1) > /dev/null
-
-start=$SECONDS
+type=$(printf "%s\n" "${typearr[@]}" | shuf -n1)
 
 read -p "$type"$'\n' typed
 
@@ -16,13 +14,11 @@ if [[ "$typed" == "$type" ]];
 then
   tput setaf 46
   echo "Good job!"
-  echo $(( SECONDS - start ))
   echo "Press enter to quit: "
   read delay
 else
   tput setaf 196
   echo "Wrong. Try again next time!"
-  echo $(( SECONDS - start ))
   echo "Press enter to quit: "
   read delay
 fi

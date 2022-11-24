146ec28a9d5cfa272a2408db12fc2ae41021f9e7
Leafboi <110484176+Leafboi111@users.noreply.github.com>
2022-11-24 10:58:38 -0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -6,7 +6,7 @@ typearr=("We're careful about orange ping pong balls because people might think 
 " "He used to get confused between soldiers and shoulders, but as a military man, he now soldiers responsibility.
 " "The sky is clear; the stars are twinkling." "25 years later, she still regretted that specific moment." "I cheated while playing the darts tournament by using a longbow.")
 
-type=$(printf "%s\n" "${typearr[@]}" | shuf -n1)
+type=$(printf "%s\n" "${typearr[@]}" | shuf -n1) > /dev/null
 
 start=$SECONDS
 

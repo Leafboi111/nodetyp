22488dbbe19d7a1325b99de571acd9e4e0d7c191
Leafboi <110484176+Leafboi111@users.noreply.github.com>
2022-11-24 10:33:51 -0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,7 +1,8 @@
+clear
+
 typearr=(" We're careful about orange ping pong balls because people might think they're fruit.
 " " He used to get confused between soldiers and shoulders, but as a military man, he now soldiers responsibility.
 " "The sky is clear; the stars are twinkling." "25 years later, she still regretted that specific moment." "I cheated while playing the darts tournament by using a longbow.")
 
 type=${typearr[$rand]}
-echo $type
-sleep 5
+read -p $type typed

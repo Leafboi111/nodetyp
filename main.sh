cadb61dcef2abccec6cba0f94f4ed1ad47854ede
Leafboi <110484176+Leafboi111@users.noreply.github.com>
2022-11-24 10:37:12 -0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -4,7 +4,7 @@ typearr=(" We're careful about orange ping pong balls because people might think
 " " He used to get confused between soldiers and shoulders, but as a military man, he now soldiers responsibility.
 " "The sky is clear; the stars are twinkling." "25 years later, she still regretted that specific moment." "I cheated while playing the darts tournament by using a longbow.")
 
-type=${typearr[$rand]}
+type=$[$RANDOM % ${#randarr[@]}]
 read -p "$type" typed
 
 if [[ "$typed" == "$type" ]];

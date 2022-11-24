d5649149716656d36937b3c6ff32b6a565b34c63
Leafboi <110484176+Leafboi111@users.noreply.github.com>
2022-11-24 10:55:43 -0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,7 @@
 clear
 
+tput setaf 46
+
 typearr=("We're careful about orange ping pong balls because people might think they're fruit.
 " "He used to get confused between soldiers and shoulders, but as a military man, he now soldiers responsibility.
 " "The sky is clear; the stars are twinkling." "25 years later, she still regretted that specific moment." "I cheated while playing the darts tournament by using a longbow.")
@@ -9,6 +11,8 @@ type=$(printf "%s\n" "${typearr[@]}" | shuf -n1)
 start=$SECONDS
 
 read -p "$type"$'\n' typed
+
+tput setaf 226
 
 if [[ "$typed" == "$type" ]];
 then

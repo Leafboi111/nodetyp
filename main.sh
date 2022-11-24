68dde0b4dd5dc3b3a30c89147e5d753e302ec475
Leafboi <110484176+Leafboi111@users.noreply.github.com>
2022-11-24 10:28:18 -0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,1 +1,6 @@
-echo "Hello, World!"
+typearr=(" We're careful about orange ping pong balls because people might think they're fruit.
+" " He used to get confused between soldiers and shoulders, but as a military man, he now soldiers responsibility.
+" "The sky is clear; the stars are twinkling." "25 years later, she still regretted that specific moment." "I cheated while playing the darts tournament by using a longbow.")
+
+type=${typearr[ $RANDOM % ${#typearr[@]} ]}
+echo $type
